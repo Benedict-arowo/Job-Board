60e2474587ac572fa22f3f4f112ba5b0e2697686
Benedict <benedict.arowo@gmail.com>
2023-10-23 22:10:10 +0300

    Changed the display name for Company model

--- app_jobboard/models.py
+++ app_jobboard/models.py
@@ -93,3 +93,7 @@ class Company(models.Model):
     name = models.CharField(max_length=256)
     category = models.ForeignKey(CompanyCategory, on_delete=models.SET_NULL, null=True, blank=True)
     representative = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
