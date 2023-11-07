d6c427d0bcbef4fe9ffeeb2156d4f623ddf78083
Benedict <benedict.arowo@gmail.com>
2023-11-07 14:36:37 +0300

    Minor fixes + DB migrations

--- app_jobboard/urls.py
+++ app_jobboard/urls.py
@@ -5,31 +5,31 @@ from .views_folder import company_views, company_reviews
 app_name = "jobboard"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("job/<uuid:id>", views.getJob, name="job"),
+    path("job/<uuid:id>/", views.getJob, name="job"),
     path("createJob/", views.createJob, name="createJob"),
     path("myJobs/", views.getUserJobs, name="getUserJobs"),
     path("company/", views.companies, name="companies"),
-    path("editJob/<uuid:id>", views.editJob, name="editJob"),
-    path("deleteJob/<uuid:id>", views.deleteJob, name="deleteJob"),
-    path("deleteSearch/<int:id>", views.deleteSearch, name="deleteSearch"),
-    path("bookmark/<uuid:jobId>", views.addJobToBookmark, name="addBookmark"),
+    path("editJob/<uuid:id>/", views.editJob, name="editJob"),
+    path("deleteJob/<uuid:id>/", views.deleteJob, name="deleteJob"),
+    path("deleteSearch/<int:id>/", views.deleteSearch, name="deleteSearch"),
+    path("bookmark/<uuid:jobId>/", views.addJobToBookmark, name="addBookmark"),
     path(
-        "remove_bookmark/<uuid:jobId>",
+        "remove_bookmark/<uuid:jobId>/",
         views.removeJobFromBookmark,
         name="removeBookmark",
     ),
     path("company/<uuid:id>/", company_views.company, name="company"),
     path("company/create", company_views.createCompany, name="createCompany"),
-    path("company/edit/<uuid:id>", company_views.editCompany, name="editCompany"),
-    path("company/delete/<uuid:id>", company_views.deleteCompany, name="deleteCompany"),
+    path("company/edit/<uuid:id>/", company_views.editCompany, name="editCompany"),
+    path("company/delete/<uuid:id>/", company_views.deleteCompany, name="deleteCompany"),
     path("companies/", company_views.userCompanies, name="userCompanies"),
     path(
         "company/<uuid:companyId>/createReview/",
         company_reviews.create_review,
         name="createCompanyReview",
     ),
-    path("review/edit/<int:id>", company_reviews.edit_review, name="editReview"),
-    path("review/delete/<int:id>", company_reviews.delete_review, name="deleteReview"),
+    path("review/edit/<int:id>/", company_reviews.edit_review, name="editReview"),
+    path("review/delete/<int:id>/", company_reviews.delete_review, name="deleteReview"),
 ]
 
 # TODO: Organize URLS into separate files.
